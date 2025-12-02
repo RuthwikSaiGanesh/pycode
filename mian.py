5d5ae16749078f693d067dff6b621aa6d5be25d7
RuthwikSaiGanesh <ruthwiksaiganesh@gmail.com>
2025-12-02 23:18:17 +0530

    Auto-corrected mian.py

--- mian.py
+++ mian.py
@@ -1,5 +1,7 @@
 def hello():
-print("hi")
- print("world")
 
-#error occurs, as the code is not following indentation rule
+
+print("hi")
+print("world")
+
+# error occurs, as the code is not following indentation rule
